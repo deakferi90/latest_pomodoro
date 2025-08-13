e8af6ece7e653fd7328cf040cc6939f9f2c2eaef
Ferenc Deak <deak_ferenc_jozsef@yahoo.com>
2025-08-13 10:24:32 +0300

    added color variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component, signal } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  signal,
+} from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { TimerComponent } from '../timer/timer.component';
 import { RouterModule } from '@angular/router';
@@ -19,10 +25,25 @@ import { DialogContentComponent } from '../dialog-content/dialog-content';
   ],
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor(private dialog: MatDialog) {}
+export class AppComponent implements OnInit {
+  constructor(
+    private dialog: MatDialog,
+    private el: ElementRef,
+    private renderer: Renderer2
+  ) {}
+
+  ngOnInit(): void {
+    this.renderer.setStyle(
+      this.el.nativeElement,
+      '--primary-color',
+      this.primaryColor
+    );
+  }
+
   activeBtn = signal<'pomodoro' | 'shortBreak' | 'longBreak'>('pomodoro');
   activeMode: 'pomodoro' | 'shortBreak' | 'longBreak' = 'pomodoro';
+
+  primaryColor = '#f87070';
 
   onModeChange(mode: 'pomodoro' | 'shortBreak' | 'longBreak') {
     this.activeMode = mode;
